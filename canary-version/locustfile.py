f63eebc8533c6c8c7982b76ba7787835d6303bb4
Matteo <matteo.ciccone23@gmail.com>
2024-12-30 18:43:12 +0100

    canary-version report

--- canary-version/locustfile.py
+++ canary-version/locustfile.py
@@ -2,17 +2,13 @@ from locust import HttpUser, task, between
 
 class FrontendUser(HttpUser):
     host = "http://34.27.165.38"
-    # Tempo di attesa tra ogni task (simulazione di utenti)
-    wait_time = between(1, 5)  # 1-5 secondi di attesa tra le richieste
+    wait_time = between(1, 5) 
     
-    # Task che viene eseguito quando un utente invia una richiesta HTTP
     @task
     def index(self):
-        # Sostituisci con l'IP pubblico o il dominio del tuo frontend
         self.client.get("/")
 
-    # Task aggiuntivo per simulare l'accesso alla pagina di un prodotto
+    # Task
     @task(2)
     def product_page(self):
-        # Simula la navigazione sulla pagina di un prodotto
         self.client.get("/product/OLJCESPC7Z")
